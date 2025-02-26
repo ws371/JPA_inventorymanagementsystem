78229230dff87e0e9e0516a05468637a855dbe1c
ws371 <waltteri.soininen@protonmail.com>
2025-02-26 16:41:11 -0100

    init commit

--- src/main/java/com/entities/Category.java
+++ src/main/java/com/entities/Category.java
@@ -11,6 +11,8 @@ public class Category {
     private String DESCRIPTION;
     public int getCATEGORY_ID() {
         return CATEGORY_ID;}
+    public void setCATEGORY_ID(int CATEGORY_ID) {
+        this.CATEGORY_ID = CATEGORY_ID;}
     public String getCATEGORY_NAME() {
         return CATEGORY_NAME;}
     public void setCATEGORY_NAME(String CATEGORY_NAME) {
